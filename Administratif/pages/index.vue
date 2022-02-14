d2abcbecdbacc20fe60d83e73273b67a4c303481
Yassine <yassine.souissy@gmail.com>
2022-02-14 17:33:13 +0100

    add front

--- Administratif/pages/index.vue
+++ Administratif/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="">
-  <div class="w-full h-24 bg-blue-500">
+  <div class="w-full h-24 empty-div">
   </div>
   <div class="w-full flex flex-col">
     <div class="flex justify-center mt-6 mb-6">
@@ -17,7 +17,7 @@
             :visible.sync="dialogVisible"
             width="30%">
             <el-input placeholder="Titre de l'alerte" v-model="title"></el-input>
-            <el-input placeholder="Ajoutez un commentaire" v-model="desc"></el-input>
+            <el-input placeholder="Ajoutez un commentaire" class="mt-4" v-model="desc"></el-input>
             <span slot="footer" class="dialog-footer">
               <el-button @click="dialogVisible = false">Annuler</el-button>
               <el-button type="primary" @click="addTask()">Ajouter</el-button>
@@ -26,20 +26,20 @@
         </div>
 
         <div class="mt-6" v-for="(item, index) in alertes.data" :key="index" >
-          <el-card class="">
+          <el-card class="content-card">
             <div class="flex flex-col">
-              <div class="flex flex-row">
+              <div class="grid justify-items-stretch">
                 <span class="text-xl font-bold">{{ item.title }}</span>
-                <span class="">{{ item.date }}</span>
+                <span class="justify-self-end date">{{ item.date }}</span>
               </div>
-              <span>{{ item.description }}</span>
+              <span class="mt-4">{{ item.description }}</span>
             </div>
           </el-card>
         </div>
       </el-card>
     </div>
   </div>
-  <div class="w-full h-24 bg-blue-500">
+  <div class="w-full h-24 empty-div">
   </div>
 </div>
 </template>
@@ -87,11 +87,28 @@ export default {
 
 <style lang="css">
 
-.content-card {
-  border-radius: 50px;
+.date {
+  background-color: rgb(212, 212, 212);
+  border-radius: 22px;
+  color: #5D6BFF;
+  padding: 8px;
 }
 
-.main-card {
+.empty-div {
+  background-color: #5D6BFF;
+}
+
+el-input {
+  @apply mt-4
+}
+
+.el-button--primary {
+  background-color: #5D6BFF;
+  border-radius: 25px;
+  border: 0px;
+}
+
+el-card {
   border-radius: 50px;
 }
 
